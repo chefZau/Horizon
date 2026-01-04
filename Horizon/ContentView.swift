da8fb906fa08f66a2b1b69d4a37987b5d7f20e85
Luca Zhou <zhoumingcong6366@gmail.com>
2026-01-03 20:55:16 -0500

    Initial Commit

---
+++ Horizon/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Horizon
+//
+//  Created by Luca Zhou on 2026-01-03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Horizon/HorizonApp.swift
@@ -0,0 +1,17 @@
+//
+//  HorizonApp.swift
+//  Horizon
+//
+//  Created by Luca Zhou on 2026-01-03.
+//
+
+import SwiftUI
+
+@main
+struct HorizonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
